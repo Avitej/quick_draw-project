af2d163b5f4f986058fbdb16c4d671e91b5ff759
Avitej <82682098+Avitej@users.noreply.github.com>
2022-05-15 16:41:03 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -12,9 +12,25 @@ function setup() {
     canvas = createCanvas(280, 280);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+}
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
 }
 function clearCanvas() {
     background("white");
+}
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+  }
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+      }
+      console.log(results);
+      drawn_sketch = results[0].label;
+      document.getElementById('your_sketch').innerHTML = 'Your Sketch: ' + drawn_sketch;
+      document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
 }
 function updateCanvas() {
     background("white");
